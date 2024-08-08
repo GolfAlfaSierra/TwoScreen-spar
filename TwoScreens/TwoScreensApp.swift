74b7205021808323223a8c68a7af1b5e8183467d
artyom <tomas.tomson@mail.ru>
2024-08-08 16:45:53 +0300

    Initial Commit

---
+++ TwoScreens/TwoScreensApp.swift
@@ -0,0 +1,17 @@
+//
+//  TwoScreensApp.swift
+//  TwoScreens
+//
+//  Created by artyom s on 08.08.2024.
+//
+
+import SwiftUI
+
+@main
+struct TwoScreensApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
